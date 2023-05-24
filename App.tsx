5b62b94bd5c908d75260a98f5ad2793ed70d8b57
Bergo <bergo@tutanota.com>
2023-05-24 18:20:54 +0300

    bottom navbar color

--- App.tsx
+++ App.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./AppNavigator";
 import { StatusBar } from "expo-status-bar";
+import { setBackgroundColorAsync } from "expo-navigation-bar";
+import { theme } from "./theme";
 
 const App = () => {
+  setBackgroundColorAsync(theme.base);
   return (
     <NavigationContainer>
       <AppNavigator />
